e86d67639543810073383862ff32bd676ddb0df1
Ang Li-Lian <anglilian@uni.minerva.edu>
2024-11-05 23:31:44 +0000

    Add base styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#372212",
+        "primary-button": "#03635D",
+        secondary: "#B55341",
+        "secondary-button": "#B7AD98",
+        rating: "#E87402",
+        background: "#F4F1EA",
+      },
+      fontFamily: {
+        lato: ["var(--font-lato)"],
+        merriweather: ["var(--font-merriweather)"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;

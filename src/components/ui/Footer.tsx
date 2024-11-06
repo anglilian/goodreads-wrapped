d5f3c120d9e1fc71f0f0b195059645742386457f
Ang Li-Lian <anglilian@uni.minerva.edu>
2024-11-06 00:04:06 +0000

    Setup first page

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,10 @@
+// src/components/ui/Footer.tsx
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-4 right-4 -z-10">
+      <p className="text-sm text-primary opacity-50">
+        goodreadswrapped.anglilian.com
+      </p>
+    </footer>
+  );
+}

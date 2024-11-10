14574ac755620e866e6da13de88f92c891f233c5
Ang Li-Lian <anglilian@uni.minerva.edu>
2024-11-10 11:34:29 +0000

    Update loader to rotate through different text

--- src/components/WelcomePage/Loader.tsx
+++ src/components/WelcomePage/Loader.tsx
@@ -1,6 +1,27 @@
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function Loader() {
+  const [textIndex, setTextIndex] = useState(0);
+
+  const loadingTexts = [
+    "Looking through your library...",
+    "Analysing your reading journey...",
+    "Comparing your books with last year...",
+    "Figuring out what books you liked...",
+  ];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTextIndex((prevIndex) =>
+        prevIndex === loadingTexts.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 10000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center space-y-2">
       <Image
@@ -10,7 +31,9 @@ export default function Loader() {
         alt="Loading animation"
         priority
       />
-      <p>Looking through your library...</p>
+      <p className="transition-opacity duration-500">
+        {loadingTexts[textIndex]}
+      </p>
     </div>
   );
 }
